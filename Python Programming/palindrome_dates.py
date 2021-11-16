59f7b09bcac4c8035b410319c9586a3a8d6910b4
kunalroy95 <59523616+kunalroy95@users.noreply.github.com>
2021-11-16 17:32:11 -0600

    Update palindrome_dates.py

--- Python Programming/palindrome_dates.py
+++ Python Programming/palindrome_dates.py
@@ -1,14 +1,4 @@
-"""
-Created on February 12th, 2021
 
-author: Kunal Roy
-
-I have not given or received any
-unauthorized assistance on this assignment
-
-Youtube Video Link: https://youtu.be/kqTgtUvHvew
-    
-"""
 
 def is_palindrome(date):
     
